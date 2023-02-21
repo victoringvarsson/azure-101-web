0ab63711086c256ccd6c998baea13edee5608cbd
Victor Ingvarsson <victor.ingvarsson@axis.com>
2023-02-21 15:01:55 +0100

    latest with api baseurl

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,9 @@ export interface Environment {
 }
 
 const prod: Environment = {
-  baseUrl: "http://localhost:7071",
+  // baseUrl: "http://localhost:7071",
+  baseUrl: "https://api.victori.course.connect.axis.com",
+  // baseUrl: "https://func-http-get-joke.azurewebsites.net",
 }
 
 export const environment: Environment = prod
